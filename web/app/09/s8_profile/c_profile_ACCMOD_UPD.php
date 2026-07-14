a80e76ec2de01ba6bbf7bc3e0c391333bd354577
odzan <odzan@romindo.net>
2026-07-14 15:30:05 +0700

    cek error

--- web/app/09/s8_profile/c_profile_ACCMOD_UPD.php
+++ web/app/09/s8_profile/c_profile_ACCMOD_UPD.php
@@ -38,7 +38,6 @@ if ($isRun) {
                 VALUES
                     ('%s', '%s', '%s', '%s'); ",
                         $varProfileID, $varAppID, $key, $val);
-                       print_r($tmpQueryString);
                 if ($varMySqli->query($tmpQueryString)) {
                     $data['status'] = true;
                 } else {
